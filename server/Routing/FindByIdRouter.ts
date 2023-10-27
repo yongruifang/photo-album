4c5d140182ccd3e54aae7f6fb77729bfd6224134
yongruifang <yongruifang@163.com>
2023-10-27 19:05:05 +0800

    Work

--- server/Routing/FindByIdRouter.ts
+++ server/Routing/FindByIdRouter.ts
@@ -4,13 +4,14 @@ import { Request, Response } from "express";
 
 export class FindByIdRouter implements IRouter {
     public AddRoute(route: any): void {
-        route.get('/id/:id', (request: Request, response: Response) => {
-            Picture.findOne({ _id: request.params.id }, '-_id', (err: any, picture: any) => {
-                if (err) {
-                    response.send(err);
-                }
-                response.json(picture);
-            });
+        route.get('/id/:id', async (request: Request, response: Response) => {
+            try {
+                const result = await Picture.findOne({ _id: request.params.id }, '-_id');
+                response.send(result)
+            } catch (err) {
+                response.send(err)
+            }
+
         });
     }
 }
--- server/Routing/GetPicturesRouter.ts
+++ server/Routing/GetPicturesRouter.ts
@@ -3,13 +3,19 @@ import { Picture } from "../Database";
 import { Response } from "express";
 export class GetPicturesRouter implements IRouter {
     public AddRoute(route: any): void {
-        route.get('/get/', (request: Request, response: Response) => {
-            Picture.distinct("_id", (err: any, picture: any) => {
-                if (err) {
-                    response.send(err);
-                }
-                response.send(picture);
-            });
+        route.get('/get/', async (request: Request, response: Response) => {
+            // Picture.distinct("_id", (err: any, picture: any) => {
+            //     if (err) {
+            //         response.send(err);
+            //     }
+            //     response.send(picture);
+            // });
+            try {
+                const result = await Picture.distinct("_id")
+                response.send(result)
+            } catch (err) {
+                response.send(err)
+            }
         });
     }
 }

097091701313c62d8466cfb2750f22667d1a29f5
yongruifang <yongruifang@163.com>
2023-10-27 13:55:44 +0800

    init

--- README.md
+++ README.md
@@ -25,3 +25,20 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+
+# 创建应用程序
+`npm install -g @angular/cli`
+创建本项目
+`ng new photoalbum --style scss --prefix pa`
+接受默认的安装配置，一路回车
+> SCSS为使用样式混入等提供了丰富的语法
+启动Angular服务器，监视文件是否发生变化
+`ng serve --open`
+端口4200，显示默认Angular示例页面
+![界面](./src/assets/ui-init.png)
+# 安装Angular Material
+```bash
+ng add @angular/material @angular/cdk @angular/animation @angular/flex-layout
+```
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LayoutModule } from '@angular/cdk/layout'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
 @NgModule({
+  // 告诉Angular我们开发了哪些组件
+  // 当使用Angular CLI来生成组件时，组件的声明自动添加到了这个数组中
   declarations: [
     AppComponent
   ],
+  // 需要在应用程序中导入哪些外部模块
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
